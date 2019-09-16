d29eb15e52a4fea707eb2235153466f39ee0794b
jaewoong <jaewoong.go@gmail.com>
2019-09-16 17:53:29 +0900

    PyPi 배포를 위한 setup.py 추가

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name             = 'django-preview-card',
+    version          = '1.0.0',
+    description      = 'Anyone can obtain data(url, image, title, description, etc) easily from plain text for showing preview card like Twitter, Facebook newsfeed',
+    long_description = open('README.md').read(),
+    author           = 'Jaewoong',
+    author_email     = 'jaewoong.go@gmail.com',
+    url              = 'https://github.com/jwkcp/django-preview-card',
+    download_url     = 'https://github.com/jwkcp/django-preview-card/#files',
+    install_requires = ['beautifulsoup4'],
+    packages         = find_packages(exclude = ['docs', 'example']),
+    keywords         = ['django', 'cardview', 'newsfeed'],
+    python_requires  = '>=3',
+    zip_safe=False,
+    classifiers      = [
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ]
+)
